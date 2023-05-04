9f6c437d2e030fe08153c09c2d156be877d117b5
TJW <jaigerjakgames@gmail.com>
2023-05-03 21:43:02 -0400

    Minor Updates. Probably going to revert.

---
+++ Data/Models/PageRoutes.cs
@@ -0,0 +1,12 @@
+﻿namespace GameUpdateBlazorUI.Data.Models
+{
+    public  class PageRoutes
+    {
+        public  string Index { get; private set; } = "";
+        public  string About { get; private set; } = "about";
+        public  string Social { get; private set; } = "social";
+        public  string SupportUs { get; private set; } = "support-us";
+        public  string Games { get; private set; } = "games";
+        public  string GameUpdates { get; private set; } = "game-updates";
+    }
+}
